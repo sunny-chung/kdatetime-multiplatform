cb8238f0cd12bb4287db07d5e2a0e95b0713f82c
Sunny Chung <sunnychung@live.hk>
2024-03-12 18:17:35 +0800

    disable native targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,15 +90,15 @@ kotlin {
     }
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> null // macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> {
-            println("Warning: Host OS is not supported in Kotlin/Native.")
-            null
-        }
-    }
+//    val nativeTarget = when {
+//        hostOs == "Mac OS X" -> null // macosX64("native")
+//        hostOs == "Linux" -> linuxX64("native")
+//        isMingwX64 -> mingwX64("native")
+//        else -> {
+//            println("Warning: Host OS is not supported in Kotlin/Native.")
+//            null
+//        }
+//    }
 
     
     sourceSets {
@@ -159,10 +159,10 @@ kotlin {
         }
         val jsMain by getting
         val jsTest by getting
-        if (nativeTarget != null) {
-            val nativeMain by getting
-            val nativeTest by getting
-        }
+//        if (nativeTarget != null) {
+//            val nativeMain by getting
+//            val nativeTest by getting
+//        }
 
         configure(darwinTargets) {
             val (mainSourceSet, testSourceSet) = when {
