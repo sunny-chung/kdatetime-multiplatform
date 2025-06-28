cdfb2b0a9451fa95f0ca79cc198610ada3b4acac
IENGROUND <ericano.rhee@gmail.com>
2025-06-28 11:02:22 +0900

    change `modPositive` into infix function

--- src/commonMain/kotlin/com/sunnychung/lib/multiplatform/kdatetime/KDateTimeFormattable.kt
+++ src/commonMain/kotlin/com/sunnychung/lib/multiplatform/kdatetime/KDateTimeFormattable.kt
@@ -3,32 +3,29 @@ package com.sunnychung.lib.multiplatform.kdatetime
 interface KDateTimeFormattable {
     fun toMilliseconds(): Long
 
-    private fun modPositive(x: Long, y: Long): Long {
-        val mod = x % y
-        return if (mod < 0) mod + y else mod
-    }
+    private infix fun Long.modPositive(y: Long): Long = (this % y).let { if (it < 0) it + y else it }
 
     fun hourPart(): Int {
         val ms = toMilliseconds()
-        val timePartMs = modPositive(ms, KFixedTimeUnit.Day.ratioToMillis)
+        val timePartMs = ms modPositive KFixedTimeUnit.Day.ratioToMillis
         return (timePartMs / KFixedTimeUnit.Hour.ratioToMillis).toInt()
     }
 
     fun minutePart(): Int {
         val ms = toMilliseconds()
-        val timePartMs = modPositive(ms, KFixedTimeUnit.Hour.ratioToMillis)
+        val timePartMs = ms modPositive KFixedTimeUnit.Hour.ratioToMillis
         return (timePartMs / KFixedTimeUnit.Minute.ratioToMillis).toInt()
     }
 
     fun secondPart(): Int {
         val ms = toMilliseconds()
-        val timePartMs = modPositive(ms, KFixedTimeUnit.Minute.ratioToMillis)
+        val timePartMs = ms modPositive KFixedTimeUnit.Minute.ratioToMillis
         return (timePartMs / KFixedTimeUnit.Second.ratioToMillis).toInt()
     }
 
     fun millisecondPart(): Int {
         val ms = toMilliseconds()
-        val timePartMs = modPositive(ms, KFixedTimeUnit.Second.ratioToMillis)
+        val timePartMs = ms modPositive KFixedTimeUnit.Second.ratioToMillis
         return timePartMs.toInt()
     }
 
